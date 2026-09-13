6aa5d49b908fcd4349c791b3064a471c81c1485d
Marina Pimenova <mnfpma@gmail.com>
2026-09-13 16:48:31 +0300

    TI-25: Extend 'question_projection' by dates, add tag<->question_count, add find-questions-by-pattern

--- sql/_06_misc.sql
+++ sql/_06_misc.sql
@@ -1,9 +1,29 @@
+select * from databasechangelog;
+
+select * from knowledge.questions_aggregated_by_tags();
+
+select * from knowledge.project;
+select * from knowledge.project_question;
+select * from knowledge.question_level;
+select * from knowledge.tag;
+select * from knowledge.question;
+--     projects     varchar(255),
+--     difficulties varchar(255),
+--     tags         varchar(255),
+--     createdby    varchar(255)
+SELECT *
+FROM knowledge.view_filtered_questions(
+        ROW('1,2', '1,2,3,4', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,36', 'SYSTEM')::knowledge.filter_params_type
+     );
+
+select * from knowledge.find_questions_by_pattern('java');
+
 select qdp.id,
-       qdp.tag,
+       qdp.tags,
        qdp.question,
        qdp.shortAnswer,
-       qdp.resourceUrl,
-       qdp.description,
+       qdp.resources,
+       --qdp.description,
        qdp.projectName,
        qdp.updatedBy
 from knowledge.question_dashboard_projection qdp
